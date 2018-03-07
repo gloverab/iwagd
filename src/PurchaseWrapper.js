c5c363b799cae0cc19da5938162655f0f55e3c4a
Alex Glover <alex@aglover.local>
2018-03-06 20:21:06 -0500

    purchase

--- src/PurchaseWrapper.js
+++ src/PurchaseWrapper.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import pin from './images/Blue-Hair.png';
 
 class PurchaseWrapper extends Component {
   constructor(props) {
@@ -13,15 +14,45 @@ class PurchaseWrapper extends Component {
       this.setState({
         rollVinyl: true
       })
-    }, 500)
+    }, 200)
   }
 
   render() {
     return (
       <div>
-        <div className='vinyl-wrapper'>
-          <img src={this.props.albumCover} alt='album-cover' className={`ib vinyl-roll album-cover ${this.state.rollVinyl ? 'shift' : ''}`} />
-          <img src={this.props.vinyl} alt='vinyl' className={`ib vinyl-roll vinyl ${this.state.rollVinyl ? 'shift' : ''}`} />
+        <div classname='album-wrapper'>
+          <div className='vinyl-wrapper'>
+            <img src={this.props.albumCover} alt='album-cover' className={`ib vinyl-roll album-cover ${this.state.rollVinyl ? 'shift' : ''}`} />
+            <img src={this.props.vinyl} alt='vinyl' className={`ib vinyl-roll vinyl ${this.state.rollVinyl ? 'shift' : ''}`} />
+          </div>
+
+          <div className='product-details'>
+            <div className='title-and-price'>
+              <h3 className='ib'>It Was A Good Dream - LP</h3>
+              <h3 className='ib right'>$20.00</h3>
+            </div>
+            <div className='two-thirds'>
+              <p className='track-list'>Track list</p>
+              <p>1. Forgetting How To Speak</p>
+              <p>2. Words Escape. Voices Emerge.</p>
+              <p>3. Grig.</p>
+              <p>4. You Left a Letter and a Song.</p>
+              <p>5. A Blistering Reminder of Why You Are Where You Are</p>
+            </div>
+
+            <div className='add-to-cart'>
+              <div className='ib two-thirds left'>
+                <h5>Digital download of "Forgetting How To Speak" available immediately with preorder</h5>
+              </div>
+              <div className='ib right'>
+                <button className='btn mid'>Add To Cart</button>
+              </div>
+            </div>
+          </div>
+
+          <div className='product-details'>
+            <img src={pin} />
+          </div>
         </div>
       </div>
     )
