32d211bebb027ad7bc68a3b5ca95c8f255440e52
Alex Glover <alex@aglover.local>
2019-07-25 12:05:31 -0400

    added press to about section

--- src/About.js
+++ src/About.js
@@ -4,18 +4,47 @@ export default class About extends React.Component {
   render() {
     return (
       <div>
-        <p>ABOUT</p>
-        <p>
-          IT WAS A GOOD DREAM is a Boston-based instrumental/post-rock band which began in early 2018. The project began as a compositional exercise for longtime friends and collaborators Chris Anthony and Alex Glover, and quickly grew into something larger than either anticipated.
-        </p>
+        <div className='about-column'>
+          <p>ABOUT</p>
+          <p>
+            IT WAS A GOOD DREAM is a Boston-based instrumental/post-rock band which began in early 2018. The project began as a compositional exercise for longtime friends and collaborators Chris Anthony and Alex Glover, and quickly grew into something larger than either anticipated.
+          </p>
 
-        <p>
-          Recorded in a centuries-old Masonic Temple-turned recording studio in Northeastern Massachusetts, the duo’s self-titled debut is saturated with a brightly haunted atmosphere. The historical live room graciously left its fingerprints all over the recordings - gorgeously architected 50-foot tall ceilings end where effect pedals begin. Drums shake unreachable shelves and cymbals crash in their wake, bringing to life the resident dust in long-forgotten hallways. Unplanned creaks and rattles invite the listener to explore each deeply layered sound expertly captured by producer James Bridges (Caspian, American Echoes, Mike Gordon).
-        </p>
-        
-        <p>
-          With their debut record due out in Summer 2018, IT WAS A GOOD DREAM will spend the spring completing a new live performance complimented by generative visuals and rotating accompaniment. In the meantime, the album's first track and teaser single, "Forgetting How To Speak" is currently streaming everywhere.
-        </p>
+          <p>
+            Recorded in a centuries-old Masonic Temple-turned recording studio in Northeastern Massachusetts, the duo’s self-titled debut is saturated with a brightly haunted atmosphere. The historical live room graciously left its fingerprints all over the recordings - gorgeously architected 50-foot tall ceilings end where effect pedals begin. Drums shake unreachable shelves and cymbals crash in their wake, bringing to life the resident dust in long-forgotten hallways. Unplanned creaks and rattles invite the listener to explore each deeply layered sound expertly captured by producer James Bridges (Caspian, American Echoes, Mike Gordon).
+          </p>
+          
+          <p>
+            With their debut record due out in Summer 2018, IT WAS A GOOD DREAM will spend the spring completing a new live performance complimented by generative visuals and rotating accompaniment. In the meantime, the album's first track and teaser single, "Forgetting How To Speak" is currently streaming everywhere.
+          </p>
+        </div>
+        <div className='about-column'>
+          <p>PRESS</p>
+          <p className='italic'>
+            "Rarely does it occur, that an album can transpose me into a certain atmosphere, irrespective of the state of mind I had when I started listening to the album."
+            -<a href='https://everythingisnoise.net/reviews/it-was-a-good-dream-help-me-to-recollect/' target='_blank'>Everything is Noise</a>
+          </p>
+
+          <p className='italic'>
+            "Pure despondence and hefty mood weaved into just over five minutes of touching post-rock"
+            -<a href='https://www.heavyblogisheavy.com/2019/04/26/exclusive-premiere-its-time-to-get-melancholic-with-it-was-a-good-dream/' target='_blank'>Heavy Blog is Heavy</a> (single premiere)
+          </p>
+          
+          <p className='italic'>
+            "This is one of those elaborate anthems which make post rock such an adored genre."
+            -<a href='http://www.merchantsofair.com/albums/it-was-a-good-dream-help-me-to-recollect' target='_blank'>Merchants of Air</a>
+          </p>
+
+          <p className='italic'>
+            "...the band seem to have arrived at a conclusion that sheer intensity (which does occasionally pop up) doesn’t have to be defined by conventional limits. Instead, it’s something that can be welcomed as part of a broader, beautiful picture like the one It Was A Good Dream paints on Help Me to Recollect. 5/5"
+            -<a href='https://capturedhowls.com/2019/05/27/it-was-a-good-dreams-new-post-rock-hits-with-captivating-emotional-freedom/' target='_blank'>Captured Howls</a>
+          </p>
+
+          <p className='italic'>
+            "One of our favorite albums of the year so far"
+            -<a href='https://www.youtube.com/watch?v=8T_aAXtMcJ4&feature=youtu.be' target='_blank'>wherepostrockdwells</a> (album premiere)
+          </p>
+        </div>
       </div>
     )
   }
